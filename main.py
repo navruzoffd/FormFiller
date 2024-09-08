470c7d7bc8e51c504ab1689e90825f6e7348bc11
navruzoffd <navruzovdaniyal04@gmail.com>
2024-09-08 21:33:37 +0300

    add weight choise

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ async def main():
     async with async_playwright() as playwright:
         form = YandexForm(playwright)
         await form.start("https://forms.yandex.ru/u/66dd673d5056905f78e71678/")
-        await form.get_form_json("forms/test.json")
+        # await form.get_form_json("forms/test.json")
+        await form.fill_form("forms/test.json")
         await asyncio.sleep(3000)
 
 if __name__ == "__main__":
--- src/yandex.py
+++ src/yandex.py
@@ -3,6 +3,7 @@ from logger import logger
 import asyncio
 import json
 import aiofiles
+import random
 
 class YandexForm:
 
@@ -30,19 +31,21 @@ class YandexForm:
         self.page = await self.context.new_page()
         await self.page.goto(url)
         await self.page.wait_for_load_state("networkidle")
+        logger.debug("Page loaded")
 
     async def get_form_json(self, json_name):
+
         form = await self.page.query_selector(".SurveyPage")
 
         form_name = await form.query_selector(".SurveyPage-Name")
         form_name_text = (await form_name.text_content()).strip()
 
-        questions = await form.query_selector_all(".QuestionMarkup")
-
         form_data = {
             "formTitle": form_name_text,
             "questions": []
         }
+
+        questions = await form.query_selector_all(".QuestionMarkup")
 
         for question in questions:
             
@@ -61,7 +64,7 @@ class YandexForm:
             question_html = await question.inner_html()
 
             if "radiogroup" in question_html:
-                question_type = "radiogroup"
+                question_type = "radiobutton"
             elif "checkbox" in question_html:
                 question_type = "checkbox"
             else:
@@ -77,3 +80,47 @@ class YandexForm:
 
         async with aiofiles.open(json_name, 'w', encoding='utf-8') as f:
             await f.write(json_data)
+        logger.debug("JSON form loaded")
+
+    async def fill_form(self, form_json: str):
+        with open(form_json, 'r', encoding='utf-8') as file:
+            form_data = json.load(file)
+
+        questions = await self.page.query_selector_all(".QuestionMarkup")
+        question_count = 0
+
+        for question in questions:
+            options = await question.query_selector_all("label")
+            question_info = form_data["questions"][question_count]
+            question_type = question_info["questionType"]
+            option_texts = [await option.inner_text() for option in options]
+            option_weights = question_info.get("selectionWeight", {})
+
+            if question_type == "radiobutton":
+                weights = [option_weights.get(str(i + 1), 1) for i in range(len(option_texts))]
+                chosen_index = random.choices(range(len(option_texts)), weights=weights, k=1)[0]
+                chosen_option = options[chosen_index]
+                
+                button = await chosen_option.query_selector("input[type='radio']")
+                if button:
+                    await button.click()
+                    await asyncio.sleep(random.randint(200, 1500) / 1000)
+                else:
+                    logger.debug(f"Radio button not found in option {chosen_option}")
+
+            elif question_type == "checkbox":
+                weights = [option_weights.get(str(i + 1), 1) for i in range(len(option_texts))]
+                chosen_indices = random.choices(range(len(option_texts)), weights=weights, k=random.randint(1, len(option_texts)))
+                chosen_indices = list(set(chosen_indices))
+                
+                for index in chosen_indices:
+                    button = await options[index].query_selector("input[type='checkbox']")
+                    if button:
+                        await button.click()
+                        await asyncio.sleep(random.randint(200, 1500) / 1000)
+                    else:
+                        logger.debug(f"Checkbox not found in option {options[index]}")
+
+            question_count += 1
+            logger.info(f"Processed question {question_count}")
+            await asyncio.sleep(random.randint(1000, 3500) / 1000)
